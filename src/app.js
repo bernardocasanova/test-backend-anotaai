c197faec150208a90e41cda69511de6d68791280
Bernardo Casanova <bernardocasanova@gmail.com>
2022-09-08 20:02:02 -0300

    appjs and server.js files

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// Application routes
+// import request from './routes/request';
+
+class App {
+  constructor() {
+    this.app = express();
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.app.use(express.urlencoded({ extented: true }));
+    this.app.use(express.json());
+  }
+
+  routes() {
+    // this.app.use('/', request);
+  }
+}
+
+export default new App().app;
