ee58e0a98648246194ea7ff12fdf78558e38a672
Bernardo Casanova <bernardocasanova@gmail.com>
2022-09-12 14:33:34 -0300

    added comment lines

--- src/controllers/CategoryController.js
+++ src/controllers/CategoryController.js
@@ -2,11 +2,27 @@ import Category from '../models/Category';
 import Product from '../models/Product';
 
 class CategoryController {
+  /**
+   * Show all categories
+   *
+   * @param object req is an object containing information about the HTTP request
+   * @param object In response to req, you use res to send back the desired HTTP response.
+   *
+   * @return json object with categories
+  */
   async index(req, res) {
     const categories = await Category.find();
     return res.json(categories);
   }
 
+  /**
+   * Create a new category
+   *
+   * @param object req is an object containing information about the HTTP request
+   * @param object In response to req, you use res to send back the desired HTTP response.
+   *
+   * @return json object with new category or a message error
+  */
   async store(req, res) {
     try {
       const category = await Category.create(req.body);
@@ -23,6 +39,14 @@ class CategoryController {
     }
   }
 
+  /**
+   * Show a specific category
+   *
+   * @param object req is an object containing information about the HTTP request
+   * @param object In response to req, you use res to send back the desired HTTP response.
+   *
+   * @return json object with category or a message error
+  */
   async show(req, res) {
     try {
       const { id } = req.params;
@@ -55,6 +79,14 @@ class CategoryController {
     }
   }
 
+  /**
+   * Update a specific category
+   *
+   * @param object req is an object containing information about the HTTP request
+   * @param object In response to req, you use res to send back the desired HTTP response.
+   *
+   * @return json object with updated category or a message error
+  */
   async update(req, res) {
     try {
       const { id } = req.params;
@@ -88,6 +120,14 @@ class CategoryController {
     }
   }
 
+  /**
+   * Delete a specific category
+   *
+   * @param object req is an object containing information about the HTTP request
+   * @param object In response to req, you use res to send back the desired HTTP response.
+   *
+   * @return json success or error message
+  */
   async delete(req, res) {
     try {
       const { id } = req.params;
