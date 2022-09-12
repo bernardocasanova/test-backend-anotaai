53cda25c313afadb9a81a3cb0fcfcf30783ceaa8
Bernardo Casanova <bernardocasanova@gmail.com>
2022-09-12 14:01:02 -0300

    Category routes

---
+++ src/routes/category.js
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import CategoryController from '../controllers/CategoryController';
+
+const router = new Router();
+
+router.get('/', CategoryController.index);
+router.get('/:id', CategoryController.show);
+router.post('/', CategoryController.store);
+router.put('/:id', CategoryController.update);
+router.delete('/:id', CategoryController.delete);
+
+export default router;
